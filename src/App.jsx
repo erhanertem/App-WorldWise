81c019e481dcb0d6de1b1a2da04c070abf8f5e46
Erhan Ertem <eertem@gmail.com>
2023-07-05 11:30:57 +0300

    Setup more routes and establish navigation across pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Product from './pages/Product'
 import Pricing from './pages/Pricing'
-import Homepage from './pages/Homepage'
 import PageNotFound from './pages/PageNotFound'
 import AppLayout from './pages/AppLayout'
+import Homepage from './pages/Homepage'
+import Login from './pages/Login'
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Homepage />} />
-				<Route path="product" element={<Product />} />
 				<Route path="pricing" element={<Pricing />} />
+				<Route path="product" element={<Product />} />
+				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />} />
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
