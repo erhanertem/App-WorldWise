782e8302b6a72f24a73930978f90770b82f9ac19
Erhan Ertem <eertem@gmail.com>
2023-07-05 16:40:33 +0300

    Add nested routes @ app sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,20 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Homepage />} />
+				{/* Declare an index/default route */}
+				<Route index element={<Homepage />} />
+				{/* <Route path="/" element={<Homepage />} /> */}
 				<Route path="pricing" element={<Pricing />} />
 				<Route path="product" element={<Product />} />
 				<Route path="login" element={<Login />} />
-				<Route path="app" element={<AppLayout />} />
+				<Route path="app" element={<AppLayout />}>
+					{/* Declare an index/default route */}
+					<Route index element={<p>List of Cities</p>} />
+					{/* Create nested routes */}
+					<Route path="cities" element={<p>List of Cities</p>} />
+					<Route path="countries" element={<p>Countries</p>} />
+					<Route path="form" element={<p>Form</p>} />
+				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styles from './Sidebar.module.css'
 import Logo from './Logo'
 import AppNav from './AppNav'
+import { Outlet } from 'react-router-dom'
 
 function Sidebar() {
 	return (
@@ -8,7 +9,9 @@ function Sidebar() {
 			<Logo />
 			<AppNav />
 
-			<p>List of Cities</p>
+			{/* React Router Outlet element that displays the child routes @ the /app route */}
+			<Outlet />
+
 			<footer className={styles.footer}>
 				<p className={styles.copyright}>
 					&copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
