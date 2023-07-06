3ec75ed08655a8210b0ebf9da23575caca327f76
Erhan Ertem <eertem@gmail.com>
2023-07-06 16:58:25 +0300

    Implement countries list @ applayout sidebar

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,12 +1,29 @@
-import styles from "./CountryItem.module.css";
+/* eslint-disable react/prop-types */
+import styles from './CountryItem.module.css'
 
-function CountryItem({ country }) {
-  return (
-    <li className={styles.countryItem}>
-      <span>{country.emoji}</span>
-      <span>{country.country}</span>
-    </li>
-  );
+const flagEmojiToPNG = flag => {
+	const countryCode = Array.from(flag, codeUnit => codeUnit.codePointAt())
+		.map(char => String.fromCharCode(char - 127397).toLowerCase())
+		.join('')
+	// console.log('⛔', countryCode)
+	// G = ASCII 71 + 127397 = UNICODE 127468 (0x1F1EC) = 🇬
+	// B = ASCII 66 + 127397 = UNICODE 127463 (0x1F1E7) = 🇧
+
+	return (
+		<img
+			src={`https://flagcdn.com/24x18/${countryCode}.png`}
+			alt="country flag"
+		/>
+	)
 }
 
-export default CountryItem;
+function CountryItem({ country }) {
+	return (
+		<li className={styles.countryItem}>
+			<span>{flagEmojiToPNG(country.emoji)}</span>
+			<span>{country.country}</span>
+		</li>
+	)
+}
+
+export default CountryItem
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/prop-types */
+import Spinner from './Spinner'
+import CountryItem from './CountryItem'
+import Message from './Message'
+import styles from './CountryList.module.css'
+
+function CountryList({ cities, isLoading }) {
+	if (isLoading) return <Spinner />
+
+	if (!cities.length)
+		return (
+			<Message message="Add your first city by clicking on a city on the map" />
+		)
+
+	// UNIQUE COUNTRY LIST
+	// -> Finding unique cities - Method #1
+	// const countries = cities.reduce((array, city) => {
+	// 	if (!array.map(el => el.country).includes(city.country)) {
+	// 		return [...array, { country: city.country, emoji: city.emoji }]
+	// 	} else {
+	// 		return array
+	// 	}
+	// }, [])
+
+	// return (
+	// 	<ul className={styles.countryList}>
+	// 		{countries.map(country => (
+	// 			<CountryItem country={country} key={country.id} />
+	// 		))}
+	// 	</ul>
+	// )
+
+	// -> Finding unique cities - Method #2
+	const countriesUnique = new Set(
+		cities.map(city =>
+			JSON.stringify({ country: city.country, emoji: city.emoji }),
+		),
+	)
+	const countries = [...countriesUnique].map(each => JSON.parse(each))
+
+	return (
+		<ul className={styles.countryList}>
+			{countries.map(country => {
+				return <CountryItem country={country} key={country.emoji} />
+			})}
+		</ul>
+	)
+}
+
+export default CountryList
