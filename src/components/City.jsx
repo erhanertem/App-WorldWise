bacaf2973ccad4aa0e32c90d10b525e7a57a7038
Erhan Ertem <eertem@gmail.com>
2023-07-10 23:58:10 +0300

    Implement current location button functionality

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import styles from './City.module.css'
 import { useParams, useSearchParams } from 'react-router-dom'
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,12 +11,20 @@ import {
 } from 'react-leaflet'
 import { useState, useEffect } from 'react'
 import { useCities } from '../contexts/CitiesContext'
+import { useGeolocation } from '../hooks/useGeoLocation'
+import Button from './Button'
+
 import flagEmojiToPNG from './flagEmojiToPNG'
 
 function Map() {
 	const { cities } = useCities()
 	const [mapPosition, setMapPosition] = useState([40, 0])
 	const [searchParams, setSearchParams] = useSearchParams()
+	const {
+		isLoading: isLoadingPosition,
+		position: geoLocationPosition,
+		getPosition,
+	} = useGeolocation()
 
 	const mapLat = searchParams.get('lat')
 	const mapLng = searchParams.get('lng')
@@ -28,8 +36,19 @@ function Map() {
 		[mapLat, mapLng],
 	)
 
+	useEffect(
+		function () {
+			if (geoLocationPosition)
+				setMapPosition([geoLocationPosition.lat, geoLocationPosition.lng])
+		},
+		[geoLocationPosition],
+	)
+
 	return (
 		<div className={styles.mapContainer}>
+			<Button type="position" onClick={getPosition}>
+				{isLoadingPosition ? 'Loading...' : 'Use your position'}
+			</Button>
 			<MapContainer
 				className={styles.map}
 				center={mapPosition}
