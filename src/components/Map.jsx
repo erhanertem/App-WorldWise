4c85c8faf26bf9d543de77688d5dd6585f43cc3a
Erhan Ertem <eertem@gmail.com>
2023-07-10 18:04:06 +0300

    Change map focus based on clicked city

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,27 @@
 /* eslint-disable no-unused-vars */
 import styles from './Map.module.css'
 import { useSearchParams, useNavigate } from 'react-router-dom'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import { useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { useState, useEffect } from 'react'
 import { useCities } from '../contexts/CitiesContext'
 import flagEmojiToPNG from './flagEmojiToPNG'
 
 function Map() {
-	const navigate = useNavigate()
+	// const navigate = useNavigate()
 
 	const { cities } = useCities()
-	const [mapPosition, setMapPosiiton] = useState([40, 0])
+	const [mapPosition, setMapPosition] = useState([40, 0])
 	const [searchParams, setSearchParams] = useSearchParams()
 
-	const lat = searchParams.get('lat')
-	const lng = searchParams.get('lng')
+	const mapLat = searchParams.get('lat')
+	const mapLng = searchParams.get('lng')
+
+	useEffect(
+		function () {
+			if (mapLat && mapLng) setMapPosition([mapLat, mapLng])
+		},
+		[mapLat, mapLng],
+	)
 
 	return (
 		<div className={styles.mapContainer}>
@@ -39,9 +46,16 @@ function Map() {
 						</Popup>
 					</Marker>
 				))}
+				<ChangeCenter position={mapPosition} />
 			</MapContainer>
 		</div>
 	)
+}
+
+function ChangeCenter({ position }) {
+	const map = useMap()
+	map.setView(position)
+	return null
 }
 
 export default Map
