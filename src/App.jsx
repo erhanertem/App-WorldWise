5e8c083dcf7b5dc2283ec4361b1a53442bae505c
Erhan Ertem <eertem@gmail.com>
2023-07-12 17:57:04 +0300

    Establish login auth

--- src/App.jsx
+++ src/App.jsx
@@ -10,34 +10,37 @@ import City from './components/City'
 import CountryList from './components/CountryList'
 import Form from './components/Form'
 import { CitiesProvider } from './contexts/CitiesContext'
+import { AuthProvider } from './contexts/FakeAuthContext'
 
 function App() {
 	return (
-		<CitiesProvider>
-			<BrowserRouter>
-				<Routes>
-					{/* Declare an index/default route */}
-					<Route index element={<Homepage />} />
-					{/* <Route path="/" element={<Homepage />} /> */}
-					<Route path="pricing" element={<Pricing />} />
-					<Route path="product" element={<Product />} />
-					<Route path="login" element={<Login />} />
-					<Route path="app" element={<AppLayout />}>
-						{/* Declare an index/default route - redirects by default using navigate hook*/}
-						<Route index element={<Navigate replace to="cities" />} />
-						{/* replace makes it a loose navigate meaning it will let you back off in the browser history */}
-						{/* Create nested routes */}
-						<Route path="cities" element={<CityList />} />
-						{/* Nested Route with id params */}
-						<Route path="cities/:id" element={<City />} />
-						{/* Create nested routes */}
-						<Route path="countries" element={<CountryList />} />
-						<Route path="form" element={<Form />} />
-					</Route>
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</BrowserRouter>
-		</CitiesProvider>
+		<AuthProvider>
+			<CitiesProvider>
+				<BrowserRouter>
+					<Routes>
+						{/* Declare an index/default route */}
+						<Route index element={<Homepage />} />
+						{/* <Route path="/" element={<Homepage />} /> */}
+						<Route path="pricing" element={<Pricing />} />
+						<Route path="product" element={<Product />} />
+						<Route path="login" element={<Login />} />
+						<Route path="app" element={<AppLayout />}>
+							{/* Declare an index/default route - redirects by default using navigate hook*/}
+							<Route index element={<Navigate replace to="cities" />} />
+							{/* replace makes it a loose navigate meaning it will let you back off in the browser history */}
+							{/* Create nested routes */}
+							<Route path="cities" element={<CityList />} />
+							{/* Nested Route with id params */}
+							<Route path="cities/:id" element={<City />} />
+							{/* Create nested routes */}
+							<Route path="countries" element={<CountryList />} />
+							<Route path="form" element={<Form />} />
+						</Route>
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</BrowserRouter>
+			</CitiesProvider>
+		</AuthProvider>
 	)
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,37 @@
 import PageNav from '../components/PageNav'
 import styles from './Login.module.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Button from '../components/Button'
+import { useAuth } from '../contexts/FakeAuthContext'
+import { useNavigate, redirect } from 'react-router-dom'
 
 export default function Login() {
 	// PRE-FILL FOR DEV PURPOSES
 	const [email, setEmail] = useState('jack@example.com')
 	const [password, setPassword] = useState('qwerty')
 
+	const { login, isAuthenticated } = useAuth() //Use login f from authContext api
+	const navigate = useNavigate()
+
+	function handleSubmit(event) {
+		event.preventDefault()
+
+		if (email && password) login(email, password)
+	}
+
+	useEffect(
+		function () {
+			if (isAuthenticated) {
+				navigate('/app', { replace: true }) //VERY IMPORTANT!! navigate option of replace is important in order to be able tio go back to app landing page with browser back button
+			}
+		},
+		[isAuthenticated, navigate],
+	)
+
 	return (
 		<main className={styles.login}>
 			<PageNav />
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={handleSubmit}>
 				<div className={styles.row}>
 					<label htmlFor="email">Email address</label>
 					<input
@@ -32,7 +53,7 @@ export default function Login() {
 				</div>
 
 				<div>
-					<button>Login</button>
+					<Button type="primary">Login</Button>
 				</div>
 			</form>
 		</main>
