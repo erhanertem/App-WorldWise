41b63f84efe993426c4d0912ed8453d17f00cc54
Erhan Ertem <eertem@gmail.com>
2024-09-07 19:20:59 +0300

    Install and create leaflet map on the map section of the app layout

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 function Map() {
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get('lat');
@@ -15,17 +19,22 @@ function Map() {
         navigate('form');
       }}
     >
-      <h1>Map</h1>
-      <h1>
-        Position: {lat} | {lng}
-      </h1>
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 23, lng: 23 });
-        }}
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Change Position
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
