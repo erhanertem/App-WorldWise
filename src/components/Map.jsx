dea980490ac28208da0f4a1a8424342772918453
Erhan Ertem <eertem@gmail.com>
2024-09-07 22:20:00 +0300

    Pinpoint the locations of the cities data on the leaflet map with corresponding popup messages

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
+import { useCities } from '../contexts/citiesContext';
+
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParams] = useSearchParams();
@@ -29,11 +32,17 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
