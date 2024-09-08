b3ac672bb25c9faf44ad7cbab95e130f220a1b11
Erhan Ertem <eertem@gmail.com>
2024-09-08 14:30:16 +0300

    Implement 'add city' form upon click on the leaflet map via leaflet map event handler custom hook & rrc programmatic navigation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,19 +5,16 @@ import styles from './Map.module.css';
 
 import { useCities } from '../contexts/citiesContext';
 
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from 'react-leaflet';
 
 function Map() {
-  const navigate = useNavigate();
   const { cities } = useCities();
-  // THIS STATE IS CREATED SO THAT THE LAST POSITION IS KEPOT ON THE MAP EVENTHOUGH WE PRESS BACK BUTTON ON THE CITY DETAILS.
+  // THIS STATE IS CREATED SO THAT THE LAST POSITION IS KEPT ON THE MAP EVENTHOUGH WE PRESS BACK BUTTON ON THE CITY DETAILS.
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
   // Read uRL for lat lng coordinates when clicked on one of the cities in the cities tab
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const mapLat = searchParams.get('lat');
   const mapLng = searchParams.get('lng');
-
   // SYNC PARAMS LAT/LNG DATA WITH MAPPOSITION DATA SO THE POPUP IS CENTERED CORRECTLY
   useEffect(
     function () {
@@ -27,14 +24,9 @@ function Map() {
   );
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate('form');
-      }}
-    >
+    <div className={styles.mapContainer}>
       <MapContainer
-        center={[mapLat || 40, mapLng || 0]}
+        center={[mapLat || mapPosition[0], mapLng || mapPosition[1]]}
         zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
@@ -56,6 +48,7 @@ function Map() {
         ))}
 
         <ChangeCenter position={mapPosition} />
+        <DetectClickEvent />
       </MapContainer>
     </div>
   );
@@ -66,6 +59,16 @@ function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
   return null; //Every JSX should return either null or something...
+}
+// CREATE A CUSTOM REACT HOOK FOR THE REACT-LEAFLET LIBRARY TO LISTEN FOR CLICK EVENTS
+function DetectClickEvent() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => {
+      console.log(e);
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
 
 export default Map;
